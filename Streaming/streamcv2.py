45e5d30ac488bd9e37c538290237e010eaab3a79
Junyong <jypark93@kookmin.ac.kr>
2022-08-02 20:57:40 +0900

    add:streaming

---
+++ Streaming/streamcv2.py
@@ -0,0 +1,21 @@
+# cv2.py
+
+import cv2
+
+def get_stream_video():
+    # camera 정의
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        # 카메라 값 불러오기
+        success, frame = cam.read()
+
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            # frame을 byte로 변경 후 특정 식??으로 변환 후에
+            # yield로 하나씩 넘겨준다.
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(frame) + b'\r\n')
